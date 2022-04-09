d9b3387218a01077ab28dd0bd04d5b4bfa424537
linza25744 <67416250+linza25744@users.noreply.github.com>
2022-04-09 22:31:26 +0300

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,24 @@
+span.market_listing_price.market_listing_price_with_fee {
+	color: #E10505
+}
+span.market_listing_price.market_listing_price_without_fee {
+	display: block;
+	color: #08B032;
+}
+div#market_buyorder_info_details {
+	display: block !important;
+}
+#p123{
+	color: #08B032;
+}
+#b123 {
+	color: #E10505;
+}
+#p123, #b123 {
+	margin: 0;
+}
+#my-btn {
+	height: 40px;
+	font-size: 20px;
+	color: #e6ff8f !important;
+}
